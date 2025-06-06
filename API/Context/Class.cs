c54bc628cd7fb4c149db276afbe7175c3b2f7608
Congphu9824 <phamphu872004@gmail.com>
2025-06-06 21:43:52 +0700

    commit lần 3
    commit lần 4
    
    commit 1
    
    commit 2
    
    commit 3

--- API/Context/Class.cs
+++ API/Context/Class.cs
@@ -4,5 +4,13 @@
     {
         public string? Name { get; set; }
         public int Id { get; set; }
+        public int description { get; set; }
+
+        public Class(string? name, int id, int description)
+        {
+            Name = name;
+            Id = id;
+            this.description = description;
+        }
     }
 }
--- API/Mapper/Class.cs
+++ API/Mapper/Class.cs
@@ -2,5 +2,15 @@
 {
     public class Class
     {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public int Description { get; set; }
+        public Class(string? name, int id, int description)
+        {
+            Name = name;
+            Id = id;
+            Description = description;
+        }
+
     }
 }
