e0ad4ad24b6b90c24d1033cf09a3cd764516f08a
Congphu9824 <phamphu872004@gmail.com>
2025-06-08 18:39:35 +0700

    update ui
    hi 2
    
    hi 3
    
    hi 4

--- API/Services/Class.cs
+++ API/Services/Class.cs
@@ -2,5 +2,8 @@
 {
     public class Class
     {
-    }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Class() { }
+        public Class(string name, string description) {
 }
